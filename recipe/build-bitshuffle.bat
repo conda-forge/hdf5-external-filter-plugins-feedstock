26c662d2a6ee2e02171b3689f383b004d13fc180
Mark Harfouche <mark.harfouche@gmail.com>
2022-06-02 20:45:37 -0400

    ninja install

--- recipe/build-bitshuffle.bat
+++ recipe/build-bitshuffle.bat
@@ -7,7 +7,7 @@ cmake %CMAKE_ARGS% ^
   -DENABLE_BZIP2_PLUGIN=no ^
   -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
   ..  || exit /b 1
-cmake --build . --target INSTALL || exit /b 1
+ninja install || exit /b 1
 
 mkdir %PREFIX%\lib\hdf5\plugin
 copy %PREFIX%\lib\plugins\h5bshuf.dll %PREFIX%\lib\hdf5\plugin || exit /b 1
