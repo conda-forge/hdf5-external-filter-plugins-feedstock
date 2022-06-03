bb439ffd4c9083e70c3f09d22a9ea68ff68f2bd5
Mark Harfouche <mark.harfouche@gmail.com>
2022-06-02 20:20:09 -0400

    Fixup recipe for windows

--- recipe/build-bzip2.bat
+++ recipe/build-bzip2.bat
@@ -1,12 +1,12 @@
 mkdir build
 cd build
-cmake ^
-  .. ^
+cmake %CMAKE_ARGS% ^
+  -G Ninja ^
   -DENABLE_BITSHUFFLE_PLUGIN=no ^
   -DENABLE_LZ4_PLUGIN=no ^
   -DENABLE_BZIP2_PLUGIN=yes ^
   -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-  -G"Visual Studio %VS_MAJOR% %VS_YEAR% Win64" || exit /b 1
+  .. || exit /b 1
 cmake --build . --target INSTALL || exit /b 1
 
 mkdir %PREFIX%\lib\hdf5\plugin
